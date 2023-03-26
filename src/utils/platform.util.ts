58b6a03a9b574dbb20e0af3e4684ab9923917a32
Maydisease <tandongs@qq.com>
2023-03-27 00:19:20 +0800

    feat: 支持宽屏展示，优化移动设备支持

--- src/utils/platform.util.ts
+++ src/utils/platform.util.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class PlatformUtilService {
 
-  public isMobile = window.screen.width;
+  public isMobile = window.screen.width < 500;
 
   constructor() {
     console.log('platform:isMobile:', this.isMobile)
